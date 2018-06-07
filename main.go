7ff43077fc7273aff6214ed9ad747c54ffb1df25
Roberto Teixeira <robteix@gmail.com>
2018-06-07 14:51:56 -0400

    Fix go-get not finding packages
    Go get tries to read the "root" to see if it's found, we didn't
    treat this usecase.

--- main.go
+++ main.go
@@ -29,8 +29,15 @@ func handler(importPath, repo string) func(w http.ResponseWriter, req *http.Requ
 	return func(w http.ResponseWriter, req *http.Request) {
 		path := strings.TrimSuffix(req.Host+req.URL.Path, "/")
 
-		// if the requested path doesn't match
+		// go get tries the "root" with ?go-get=1
+		if path == importPath {
+			http.Redirect(w, req, "https://godoc.org/"+importPath, 302)
+			return
+		}
+
+		// requested path must be under importPath
 		if !strings.HasPrefix(path, importPath+"/") {
+			log.Printf("NOT FOUND: requested path:", path)
 			http.NotFound(w, req)
 			return
 		}
@@ -63,7 +70,7 @@ var tmpl = template.Must(template.New("main").Parse(`<!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta http-equiv="refresh" content="0; url=https://godoc.org/{{.ImportRoot}}{{.PkgPath}}">
-<meta name="go-import" content="{{.ImportRoot}} git {{.Repo}}">
+<meta name="go-import" content="{{.ImportRoot}}{{.PkgPath}} git {{.Repo}}{{.PkgPath}}">
 </head>
 <body>
 Redirecting to <a href="https://godoc.org/{{.ImportRoot}}{{.PkgPath}}">godoc.org/{{.ImportRoot}}{{.PkgPath}}</a>...
